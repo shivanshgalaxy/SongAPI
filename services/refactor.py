20eab5b1d45aafbea11569f116d4259eb2a7a020
shivanshgalaxy <shivansh.galaxy@gmail.com>
2025-06-24 01:13:13 +0530

    Add authentication to get spotify token

--- services/refactor.py
+++ services/refactor.py
@@ -1,14 +1,22 @@
 import os
 from dotenv import load_dotenv
 from spotify_auth import get_token
+from requests import get
+import json
 
 load_dotenv()
 def main():
     client_id = os.getenv("SPOTIFY_CLIENT_ID")
     client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
-    token = get_token(client_id, client_secret)
-    
-    pass
+    if not client_id or not client_secret:
+        raise EnvironmentError("Please set SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET in your .env file.")
+        return
 
-if __name__ == "__refactor__":
-    main()
+    token = get_token(client_id, client_secret)
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+
+
+main()
--- services/spotify_auth.py
+++ services/spotify_auth.py
@@ -1,3 +1,52 @@
+import base64
+from requests import post
+import os
+import json
+import time
+
+TOKEN_FILE = "token.json"
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+CONTENT_TYPE = "application/x-www-form-urlencoded"
+
+
+def save_token(token: str, expires_in: int) -> None:
+    data = {
+        "access_token": token,
+        "expires_at": time.time() + expires_in - 60
+    }
+    with open(TOKEN_FILE, "w") as f:
+        json.dump(data, f)
+
+def load_token() -> str | None:
+    if not os.path.exists(TOKEN_FILE):
+        return None
+    with open(TOKEN_FILE, "r") as f:
+        data = json.load(f)
+    if data["expires_at"] > time.time():
+        return data["access_token"]
+    return None
+
 def get_token(client_id: str, client_secret: str) -> str:
-    
-    pass
+    token = load_token()
+    if token:
+        return token
+
+    auth_string = f"{client_id}:{client_secret}"
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": CONTENT_TYPE
+    }
+    data = { "grant_type": "client_credentials" }
+    result = post(TOKEN_URL, headers=headers, data=data)
+    try:
+        result.raise_for_status()
+    except:
+        raise ValueError(f"Failed to retrieve access token: {result.status_code} {result.text}")
+    json_result = result.json()
+    token = json_result.get("access_token")
+    if not token:
+        raise ValueError("Failed to retrieve access token from Spotify API.")
+    save_token(token, json_result.get("expires_in", 3600))
+    return token
