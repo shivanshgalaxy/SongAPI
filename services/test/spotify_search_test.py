bf6ee76cabf5bbb4ff04ce5ede635b0b6664e39a
shivanshgalaxy <shivansh.galaxy@gmail.com>
2025-06-27 19:57:37 +0530

    Add test for metadata manager

--- services/test/spotify_search_test.py
+++ services/test/spotify_search_test.py
@@ -14,4 +14,12 @@ client_id = os.getenv("SPOTIFY_CLIENT_ID")
 client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 token = get_token(client_id, client_secret)
 
+# https://open.spotify.com/track/3zL0LAsSh3dTO73dSOKWkr?si=8bbf02d0b0c346ea
+
+youtube = YoutubeMetadataProvider()
+spotify = SpotifyMetadataProvider(token)
+writer = MetadataWriter()
+manager = MetadataManager([spotify, youtube], writer)
+metadata = manager.get_metadata("https://music.youtube.com/watch?v=8WuVWqyGPcs&si=OuAeaw6-il7Grllf")
+
 
